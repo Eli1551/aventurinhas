212623d77035f5055f49e8f5824b0633e4c74107
Eli1551 <00001111076613sp@al.educacao.sp.gov.br>
2024-10-02 10:35:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Feito por Eliaquim Pereira Silva Santos 2D
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
